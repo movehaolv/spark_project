43ceedac0ed9206898ac8bd2ed644ba49ad09355
lvhao <lvhao@songshuai.com>
2020-04-01 22:05:13 +0800

    update .gitignore

--- sparkmall/sparkmall-commom/src/main/scala/com/lh/datamode/ProductInfo.scala
+++ sparkmall/sparkmall-commom/src/main/scala/com/lh/datamode/ProductInfo.scala
@@ -1,5 +1,14 @@
 package com.lh.datamode
 
-object ProductInfo {
 
-}
+/**
+ * 产品表
+ *
+ * @param product_id   商品的ID
+ * @param product_name 商品的名称
+ * @param extend_info  商品额外的信息
+ */
+case class ProductInfo(product_id: Long,
+                       product_name: String,
+                       extend_info: String
+                      )
